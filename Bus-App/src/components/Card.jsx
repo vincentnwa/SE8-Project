d5b2239be91a8635e3dcf61ad1d9e05852f04933
Vincent | SE8 <vincenntnwa@me.com>
2024-10-17 23:50:55 +0800

    remove file

--- Bus-App/src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-// .\src\components\Card.jsx
-
-import { useState } from "react";
-import styles from "../components/Card.module.css";
-
-function Card({ children }) {
-  return <div className={styles.cardContainer}>{children}</div>;
-}
-export default Card;
